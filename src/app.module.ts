f1e29d5912dcc50a26f725dab56bad9d23e8fd93
Sergio Florez <ferchoivan05@hotmail.com>
2025-11-21 23:18:25 -0500

    initial configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
+// Libraries
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// Application
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Config
+import typeorm from './config/typeorm';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [typeorm],
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => config.get('typeorm') ?? {},
+        }),
+        JwtModule.register({
+            global: true,
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '9h' },
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+// Libraries
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// Modules
 import { AppModule } from './app.module';
 
+// Middleware
+import { loggerGlobal } from './middleware';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(loggerGlobal);
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    const options = new DocumentBuilder().setTitle('AGRONET - API REST').setVersion('1.0').addBearerAuth().build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+        },
+    }); // ruta http://localhost:3002/api
+
+    await app.listen(process.env.PORT ?? 3002);
+    console.log('Servidor corriendo en el puerto 3002');
 }
 bootstrap();
